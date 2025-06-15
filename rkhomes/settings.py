d24f892c3fe01a1974dcf867a06ff08c1390b77c
GuessCodee <parthkh02@gmail.com>
2025-06-15 23:41:39 +0530

    new

--- rkhomes/settings.py
+++ rkhomes/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x=ft_f28l9e$hgh4zt_(!k6_3ulkbpbxgi177@w&ha66(!kl6%'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
