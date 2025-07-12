800572946626dfe724ceafe76a04815c94ba668e
GuessCodee <parthkh02@gmail.com>
2025-07-12 15:03:07 +0530

    new

--- rkhomes/settings.py
+++ rkhomes/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['RKHomes.onrender.com','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['RKHomes.onrender.com']
 
 
 
